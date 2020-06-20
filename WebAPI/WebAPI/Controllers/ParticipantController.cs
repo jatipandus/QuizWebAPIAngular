2f860569ae6f3e833274f0756b52ebdcd4057e7e
Jati Pandu Saputra <32126782+jatipandus@users.noreply.github.com>
2020-06-21 01:16:58 +0700

    QuizWebAPIAngular

---
+++ WebAPI/WebAPI/Controllers/ParticipantController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ParticipantController : ApiController
+    {
+        [HttpPost]
+        [Route("api/InsertParticipant")]
+
+        public Tb_M_Participant Insert(Tb_M_Participant model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Tb_M_Participant.Add(model);
+                db.SaveChanges();
+                return model;
+            }
+        }
+
+        [HttpPost]
+        [Route("api/UpdateOutput")]
+        public void UpdateOutput(Tb_M_Participant model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Entry(model).State = System.Data.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
